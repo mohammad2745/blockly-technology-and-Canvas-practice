7850087101367359508490640262b3f74329edf0
mohammad2745 <mohammadfcc2745@gmail.com>
2023-06-09 06:46:03 +0600

    Basic of blockly

--- blockly-intro/blockly.js
+++ blockly-intro/blockly.js
@@ -1,16 +1,30 @@
-// Create a workspace
-var workspace = Blockly.inject('blocklyDiv', {
-    toolbox: document.getElementById('toolbox')
-});
+var workspace;
 
-// Function to run the generated JavaScript code
-function runCode() {
+// Function to initialize the Blockly workspace
+function initializeWorkspace() {
+    workspace = Blockly.inject('blocklyDiv', {
+        toolbox: document.getElementById('toolbox')
+    });
+}
+
+// Function to generate and display JavaScript code
+function generateCode() {
     // Generate JavaScript code from the workspace
     var code = Blockly.JavaScript.workspaceToCode(workspace);
 
     // Display the generated code
     document.getElementById('codeDisplay').textContent = code;
+}
+
+// Function to run the generated JavaScript code
+function runCode() {
+    // Generate JavaScript code from the workspace
+    var code = Blockly.JavaScript.workspaceToCode(workspace);
+    console.log("Code: ", code);
 
     // Execute the generated code
     eval(code);
 }
+
+// Initialize the Blockly workspace
+initializeWorkspace();
