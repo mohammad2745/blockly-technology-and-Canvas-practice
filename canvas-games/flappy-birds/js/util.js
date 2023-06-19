72b70d1032f8617b149fa2bdf2b47a98f19f0abe
mohammad2745 <mohammadfcc2745@gmail.com>
2023-06-19 12:13:19 +0600

    Global function created-updated

--- canvas-games/flappy-birds/js/util.js
+++ canvas-games/flappy-birds/js/util.js
@@ -12,11 +12,11 @@ window.requestAnimFrame = (function() {
 
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-function getRandomInt(min, max) {
+window.getRandomInt = function(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min);
-}
+    return Math.floor(Math.random() * (max - min)) + min;
+};
 
 window.getRandomColor = function() {
     var red = getRandomInt(0, 257);
