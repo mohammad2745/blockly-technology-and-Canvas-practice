6ff6e1a7542d57037c4a62e99b75596485dd3604
mohammad2745 <mohammadfcc2745@gmail.com>
2023-06-19 12:22:45 +0600

    Game object created

---
+++ canvas-games/flappy-birds/js/canvas-script.js
@@ -0,0 +1,8 @@
+window.onload = () => {
+
+    // Canvas Definition
+    let canvas = document.getElementById('flappy-monster-game');
+
+    // Game object
+    let flappyMonster = new FlappyMonster(canvas);
+}
---
+++ canvas-games/flappy-birds/js/flappy-moster.js
@@ -0,0 +1,6 @@
+function FlappyMonster(canvas) {
+    let game = this;
+
+    game.canvas = canvas;
+    game.context = game.canvas.getContext('2d');
+}
